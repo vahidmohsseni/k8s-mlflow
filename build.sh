b2d382316ec6e5550453759875f0bb3e08560253
vahidmohsseni <vahidmohsseni@yahoo.com>
2022-05-27 10:44:03 +0000

    Fix issues in build

--- build.sh
+++ build.sh
@@ -6,7 +6,10 @@ kubectl apply -f deploy/kubernetes/mlflow-namespace.yaml
 kubectl apply -f deploy/kubernetes/mlflow-deployment.yaml
 kubectl apply -f deploy/kubernetes/mlflow-service.yaml
 
+kubectl get deployments.apps -n mlflow-k8s mlflow -o jsonpath='{.status.conditions}' | echo "Waiting fot the deployment to be available ..."
+kubectl wait deployment -n mlflow-k8s mlflow --for condition=Available=True
 
 export MLFLOW_IP=$(kubectl -n mlflow-k8s get service mlflow -o jsonpath='{.spec.clusterIP}')
+export MLFLOW_PORT=$(kubectl -n mlflow-k8s get service mlflow -o jsonpath='{.spec.ports[].port}')
 
-echo MLFLOW node port ip address: $MLFLOW_IP 
+echo MLFlow service is running on: $MLFLOW_IP:$MLFLOW_PORT
